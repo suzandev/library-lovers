434f9187de84a82c7445f10df3779d8d237dc793
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-18 11:52:53 +0600

    Remove Context api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
-import Provider from "./context/Provider.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
-      <App />
-      <Toaster position="top-center" reverseOrder={true} />
-    </Provider>
+    <App />
+    <Toaster position="top-center" reverseOrder={true} />
   </React.StrictMode>,
 );
